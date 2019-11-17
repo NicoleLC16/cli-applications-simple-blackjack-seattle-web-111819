1ffa7a8d18068d47b51e0c570d35726129c6187e
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-17 00:16:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,9 +6,9 @@ def deal_card
 rand(1..11)
 end
 
-def display_card_total(cards)
+def display_card_total(card_total)
   
- return "Your cards add up to #{card_total}"
+puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
