3e4108ef495919110eac931b67ba70c0bee69943
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-17 00:40:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,7 @@ def hit?(card_total)
 end
 
 def invalid_command
-  print "Please enter a valid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
